4bddde78147cbc9a1c74be94db0d9a318c5d3dde
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-07 17:23:55 +0300

    Make logic for PageCreateForm and tests for it

--- src/forms/PageCreateForm.php
+++ src/forms/PageCreateForm.php
@@ -2,6 +2,41 @@
 
 namespace DmitriiKoziuk\yii2Pages\forms;
 
-class PageCreateForm
+use yii\base\Model;
+
+class PageCreateForm extends Model
 {
+    const ACTIVE = 1;
+
+    const NOT_ACTIVE = 0;
+
+    public $name;
+
+    public $is_active;
+
+    public $meta_title;
+
+    public $meta_description;
+
+    public $content;
+
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 150],
+            [['name'], 'trim'],
+            [['is_active'], 'integer'],
+            [['is_active'], 'default', 'value' => self::NOT_ACTIVE],
+            [['is_active'], function ($attribute) {
+                if ($this->$attribute != self::ACTIVE && $this->$attribute != self::NOT_ACTIVE) {
+                    $this->addError($attribute, "Page status code '{$this->$attribute}' not supported.");
+                }
+            }],
+            [['meta_title', 'meta_description'], 'string', 'max' => 255],
+            [['meta_title', 'meta_description'], 'trim'],
+            [['content'], 'string'],
+            [['meta_title', 'meta_description', 'content'], 'default', 'value' => null],
+        ];
+    }
 }
