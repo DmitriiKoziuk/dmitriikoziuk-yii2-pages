721e1b7985acfae2361ab5403ffff0d697a7a580
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-08 10:41:35 +0300

    Create PageRepository

---
+++ src/repositories/PageRepository.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\yii2Pages\repositories;
+
+use DmitriiKoziuk\yii2Base\repositories\AbstractActiveRecordRepository;
+use DmitriiKoziuk\yii2Pages\interfaces\PageRepositoryInterface;
+
+class PageRepository extends AbstractActiveRecordRepository implements PageRepositoryInterface
+{
+}
