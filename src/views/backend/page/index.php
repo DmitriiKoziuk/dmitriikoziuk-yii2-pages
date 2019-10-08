fc8a4c137ad3e72e1a48ee5d84fd4ea0a3576045
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-08 13:41:03 +0300

    Set is_active label status, filter drop down menu

--- src/views/backend/page/index.php
+++ src/views/backend/page/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use DmitriiKoziuk\yii2Pages\entities\PageEntity;
+use DmitriiKoziuk\yii2Pages\forms\PageUpdateForm;
 
 /* @var $this yii\web\View */
 /* @var $searchModel DmitriiKoziuk\yii2Pages\entities\PageEntitySearch */
@@ -36,7 +37,18 @@ $this->params['breadcrumbs'][] = $this->title;
                     return $model->url->url;
                 },
             ],
-            'is_active',
+            [
+                'attribute' => 'is_active',
+                'label' => 'Status',
+                'content' => function ($model) {
+                    /** @var PageEntity $model */
+                    return empty($model->is_active) ? 'Not active' : 'Active';
+                },
+                'filter' => [
+                    PageUpdateForm::NOT_ACTIVE => 'Not active',
+                    PageUpdateForm::ACTIVE => 'Active',
+                ],
+            ],
             'meta_title',
             'meta_description',
             //'content:ntext',
