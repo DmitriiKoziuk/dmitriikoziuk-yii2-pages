df77a79b9a2837b7431a4a8bce88d7ed4f730512
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-09 12:48:01 +0300

    Add select for page status (is_active)

--- src/views/backend/page/_form.php
+++ src/views/backend/page/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use DmitriiKoziuk\yii2Pages\forms\PageCreateForm;
 
 /* @var $this yii\web\View */
 /* @var $model DmitriiKoziuk\yii2Pages\forms\PageCreateForm|DmitriiKoziuk\yii2Pages\forms\PageUpdateForm */
@@ -14,11 +15,14 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'is_active')->textInput() ?>
+    <?= $form->field($model, 'is_active')->dropDownList([
+        PageCreateForm::NOT_ACTIVE => 'Not active',
+        PageCreateForm::ACTIVE => 'Active',
+    ]) ?>
 
     <?= $form->field($model, 'meta_title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'meta_description')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'meta_description')->textarea(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
