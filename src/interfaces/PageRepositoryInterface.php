099cba5eb67178b4618a8e1b70bbdf3d6e6a6577
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-09 13:12:07 +0300

    Add PageRepositoryInterface::getPageById()

--- src/interfaces/PageRepositoryInterface.php
+++ src/interfaces/PageRepositoryInterface.php
@@ -3,7 +3,9 @@
 namespace DmitriiKoziuk\yii2Pages\interfaces;
 
 use DmitriiKoziuk\yii2Base\interfaces\ActiveRecordRepositoryInterface;
+use DmitriiKoziuk\yii2Pages\entities\PageEntity;
 
 interface PageRepositoryInterface extends ActiveRecordRepositoryInterface
 {
+    public function getPageById(int $pageId): ?PageEntity;
 }
